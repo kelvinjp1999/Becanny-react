1ad0a8c4958fa6e709f01b4e68e73f7b8ecbfcb9
kelvinjp1999 <87286523+kelvinjp1999@users.noreply.github.com>
2025-09-01 20:56:39 -0300

    Feat:Posicionamento

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,32 @@ import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
 
 //Pages
+import Home from './pages/Home/Home'
+import Register from './pages/Register/Register'
+import Login from './pages/Login/Login'
+import Products from './pages/Products/Products'
+import Product from './pages/Product/Product'
+import Cart from './pages/Cart/Cart'
+import Profile from './pages/Profile/Profile'
+import NotFound from './pages/404/NotFound'
 
 function App() {
 
   return (
-    <div>
+    <BrowserRouter>
       <Navbar/>
-      <h1>Becanny e-commerce</h1>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/products' element={<Products/>}/>
+        <Route path='/product/:id' element={<Product/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/*' element={<NotFound/>}/>
+      </Routes>
       <Footer/>
-    </div>
+    </BrowserRouter>
   )
 }
 
